cdf0ea7667343030f933161fdcede735c6a16625
yvanorek <yessica.vanorek@gmail.com>
2023-06-05 22:32:30 +0200

    Initial commit

---
+++ src/main/java/com/example/maintainancemonitor/MaintainanceMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.example.maintainancemonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaintainanceMonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaintainanceMonitorApplication.class, args);
+    }
+
+}
